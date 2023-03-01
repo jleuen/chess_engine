e3b5ec14d398cca30845afc6925ae642abce97eb
jujulein <121499363+jujulein@users.noreply.github.com>
2023-03-01 17:08:22 +0100

    changement de structure des moteurs (player aux lieux de local_board)

--- chess_interface.py
+++ chess_interface.py
@@ -19,7 +19,7 @@ def game(partie):  # partie = chess.Board()
         print(partie)
         if partie.turn:  ## check if it's white turn (True) or black turn (False)
             start = time.time()
-            local_game = evaluate.Local_Board_7c(partie)
+            local_game = evaluate.Local_Board_7d(partie)
 
             print("White to play")
             best_move = local_game.best_move[0]
@@ -29,7 +29,7 @@ def game(partie):  # partie = chess.Board()
 
         else:
             start = time.time()
-            local_game = evaluate.Local_Board_7c(partie)
+            local_game = evaluate.Local_Board_7d(partie)
             print("black to play")
             best_move = local_game.best_move[0]
             print("best move is", best_move)
@@ -37,3 +37,22 @@ def game(partie):  # partie = chess.Board()
             print("time to play :", time.time() - start)
 
         print("eval is", local_game.eval)
+
+
+def an_other_game(partie):
+    while not partie.is_game_over():
+        white_player = evaluate.Local_Player_1a(partie, "white")
+        black_player = evaluate.Local_Player_1a(partie, "black")
+
+        if partie.turn:  ## check if it's white turn (True) or black turn (False)
+            print("White to play")
+            white_player.update(partie)
+            white_player.best_move()
+            best_move = white_player.best_move[0]
+            print("best move is", best_move)
+            partie.push(best_move)
+        else:
+            print("black to play")
+            best_move = black_player.best_move[0]
+            print("best move is", best_move)
+            partie.push(best_move)
--- main.py
+++ main.py
@@ -7,4 +7,4 @@ match1 = chess.Board()
 ind_2a = evaluate.Local_Board_1a(match1)
 #ind_5a = evaluate.Local_Board_5a(match1)
 
-chess_interface.game(match1)
+chess_interface.an_other_game(match1)
